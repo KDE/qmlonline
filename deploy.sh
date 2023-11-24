7e2adbb23219db19cbdbd76fbd05f8d5ccf77157
Thiago Sueto <herzenschein@gmail.com>
2023-11-24 02:49:17 +0000

    Minor fixes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 DOCKER_IMAGE=madmanfred/qt-webassembly:qt5.15-em1.39.10-remoteobjects
-EMS_CACHE=$HOME/.emscripten_cache
-SOURCE_DIR=$PWD
 
-DOCKER_COMMAND="docker run --rm -v $EMS_CACHE:/emsdk_portable/.data/cache -v $SOURCE_DIR/:/src/ -u $(id -u):$(id -g) $DOCKER_IMAGE"
+DOCKER_COMMAND="docker run --rm -v cache:/emsdk_portable/.data/cache:z -v $PWD:/src:z -u $(id -u):$(id -g) $DOCKER_IMAGE"
 
 # Build
 mkdir -p build
